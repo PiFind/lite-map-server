46bd39963ea15773f40f0d6e3baba26afa5f6f01
Zhang Ming <zmgithub@163.com>
2023-04-19 17:46:15 +0800

    增加部分注释

--- poi-service/src/main/java/io/pifind/mapserver/util/InterestPointHashUtils.java
+++ poi-service/src/main/java/io/pifind/mapserver/util/InterestPointHashUtils.java
@@ -6,6 +6,12 @@ public class InterestPointHashUtils {
 
     private final static String DEFAULT_KEY = "k3Ab3VmezvxoyfVpn1kpEF0osEUwivMv";
 
+    /**
+     * 生成 hash
+     * @param name 名字
+     * @param address 地址
+     * @return 哈希值
+     */
     public static String hash(String name,String address) {
         String hash = DigestUtils.sha256Hex(name + address + DEFAULT_KEY);
         StringBuilder hashBuilder = new StringBuilder(hash);
@@ -17,6 +23,13 @@ public class InterestPointHashUtils {
         return hashBuilder.toString();
     }
 
+    /**
+     * 检查 hash
+     * @param name 名字
+     * @param address 地址
+     * @param hash 哈希值
+     * @return 是否匹配
+     */
     public static boolean check(String name,String address, String hash) {
         return InterestPointHashUtils.hash(name,address).equals(hash);
     }
