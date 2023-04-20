acd6d5dfeabe518687776b9a9fb502b977749652
Zhang Ming <zmgithub@163.com>
2023-04-20 15:22:33 +0800

    为请求添加游客 token

--- poi-service/src/main/java/io/pifind/mapserver/remote/request/MapServiceTemplate.java
+++ poi-service/src/main/java/io/pifind/mapserver/remote/request/MapServiceTemplate.java
@@ -1,5 +1,7 @@
 package io.pifind.mapserver.remote.request;
 
+import io.pifind.authorization.model.User;
+import io.pifind.authorization.utils.JwtUtils;
 import io.pifind.common.request.StandardRestTemplate;
 import org.springframework.stereotype.Component;
 
@@ -8,5 +10,34 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class MapServiceTemplate extends StandardRestTemplate {
+
+    public MapServiceTemplate() {
+        init();
+    }
+
+    /**
+     * 初始化
+     * 因为 map-service 服务需要鉴权，所以需要在请求头中添加 token，
+     * 这里使用游客的 token，所以这里为所有请求添加游客的 token
+     */
+    private void init() {
+        getClientHttpRequestInitializers().add(httpRequest -> {
+
+            // (1) 使用 jwt 生成一个游客的 token
+            User loggedInUser = new User();
+            loggedInUser.setId(0L);
+            loggedInUser.setRole("TOURIST");
+            loggedInUser.setUsername("tourist");
+            loggedInUser.setPiPlatformToken("");
+            String loggedInUserToken = JwtUtils.generateToken(loggedInUser);
+
+            // (2) 添加 token 到请求头
+            httpRequest.getHeaders().add(
+                    "Authorization",
+                    "Bearer " + loggedInUserToken
+            );
+
+        });
+    }
 
 }
